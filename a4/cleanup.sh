c78c3078979ef71f0b0e2dc23e52de84da0bd908
hishaamg21 <143459404+hishaamg21@users.noreply.github.com>
2023-11-06 15:16:14 -0800

    a4

--- a4/cleanup.sh
+++ a4/cleanup.sh
@@ -1,2 +1,5 @@
 # removing whitespaces and newlines
 sed -e 's/"\t"/""/g' -e 's/"\n"/""/g' movies.csv
+# removing commas and pipes
+sed -e 's/",,"/","/' -e 's/",,,"/","/' -e 's/"|"/""/' movies.csv
+
